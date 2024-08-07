49bcd86baeba5d753e76ad910149255b4cbefb5e
TopBoyDrako <sinbadprince9@gmail.com>
2024-08-07 01:06:05 +0100

    task completed

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,57 +3,71 @@
 This project contains a prime game which helps in technical interviews
 """
 
-
 def isWinner(x, nums):
-    """Function to get who has won in prime game"""
-    mariaWinsCount = 0
-    benWinsCount = 0
+    # Helper function to find all primes up to max_num using Sieve of Eratosthenes
+    def sieve(max_num):
+        is_prime = [True] * (max_num + 1)
+        is_prime[0] = is_prime[1] = False  # 0 and 1 are not prime numbers
+        p = 2
+        while (p * p <= max_num):
+            if (is_prime[p] == True):
+                for i in range(p * p, max_num + 1, p):
+                    is_prime[i] = False
+            p += 1
+        primes = [p for p, prime in enumerate(is_prime) if prime]
+        return primes, is_prime
 
-    for num in nums:
-        roundsSet = list(range(1, num + 1))
-        primesSet = primes_in_range(1, num)
+    # Maximum possible value of n
+    max_n = max(nums) if nums else 0
 
-        if not primesSet:
-            benWinsCount += 1
+    # Generate all primes up to the maximum number in nums
+    primes, is_prime = sieve(max_n)
+
+    # Initialize win counters
+    maria_wins = 0
+    ben_wins = 0
+
+    # Play each round
+    for n in nums:
+        # If n is 1, Ben wins because Maria can't make a move
+        if n == 1:
+            ben_wins += 1
             continue
+        
+        # Set of remaining numbers
+        remaining = set(range(1, n + 1))
+        
+        # Simulate the game
+        turn = 0  # 0 for Maria's turn, 1 for Ben's turn
+        while True:
+            # Find the smallest prime in the remaining set
+            move_made = False
+            for prime in primes:
+                if prime > n:
+                    break
+                if prime in remaining:
+                    move_made = True
+                    # Remove the prime and its multiples from the set
+                    for multiple in range(prime, n + 1, prime):
+                        if multiple in remaining:
+                            remaining.remove(multiple)
+                    break
 
-        isMariaTurns = True
-
-        while(True):
-            if not primesSet:
-                if isMariaTurns:
-                    benWinsCount += 1
+            if not move_made:
+                # No more moves can be made
+                if turn == 0:
+                    ben_wins += 1  # Maria's turn, but she cannot move
                 else:
-                    mariaWinsCount += 1
+                    maria_wins += 1  # Ben's turn, but he cannot move
                 break
+            
+            # Switch turn
+            turn = 1 - turn
 
-            smallestPrime = primesSet.pop(0)
-            roundsSet.remove(smallestPrime)
-
-            roundsSet = [x for x in roundsSet if x % smallestPrime != 0]
-
-            isMariaTurns = not isMariaTurns
-
-    if mariaWinsCount > benWinsCount:
-        return "Winner: Maria"
-
-    if mariaWinsCount < benWinsCount:
-        return "Winner: Ben"
-
-    return None
-
-
-def is_prime(n):
-    """Returns True if n is prime, else False."""
-    if n < 2:
-        return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-
-def primes_in_range(start, end):
-    """Returns a list of prime numbers between start and end (inclusive)."""
-    primes = [n for n in range(start, end+1) if is_prime(n)]
-    return primes
+    # Determine the overall winner
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
